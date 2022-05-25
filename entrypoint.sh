4475d832c09514de8acc432966b4d52804d6a218
Egor Malyshkin <malyshkin@hightech.group>
2022-05-25 13:18:04 +0300

    fix parser py

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,6 @@ done
 mkdir outputs
 
 # Launch sherlock for solutions
-cd solutions ; ../sherlock -e cs $(echo ./*) > ../outputs/result.txt ; cd ..
+cd solutions ; ../sherlock -e .cs * $(echo ./*) > ../outputs/result.txt ; cd ..
 ls /outputs
 python3 /parser.py
