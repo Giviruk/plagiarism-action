f260ce174f616245b98605cf1d69bb25f3070403
Egor Malyshkin <malyshkin@hightech.group>
2022-05-25 13:52:13 +0300

    fix parser py

--- parser.py
+++ parser.py
@@ -56,7 +56,7 @@ def parse_plagiarism_result(path):
 
 if __name__ == '__main__':
     report = parse_plagiarism_result("outputs/result.txt")
-
+    print(report)
     keys = set()
     matrix = [[]]
 
@@ -84,4 +84,4 @@ if __name__ == '__main__':
                               cmap='coolwarm')
 
         figure = heatmap.get_figure()
-        figure.savefig("outputs/plot.png")
+        figure.savefig("./outputs/plot.png")
