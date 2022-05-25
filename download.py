57000507cf07f3fdc33d069b94e5832f48083984
Egor Malyshkin <malyshkin@hightech.group>
2022-05-25 11:20:10 +0300

    fix download py

--- download.py
+++ download.py
@@ -25,7 +25,7 @@ def download_pulls():
 
     for pr in pulls:
         branch = pr.head.ref
-        directory = f"./solutions/{pr.user.login}"
+        directory = f"./solutions/{pr.user.login}/{branch}"
         login = "Giviruk"
         password = "Trustme1*"
         remote = f"https://{login}:{password}@github.com/{REPOSITORY_NAME}.git"
